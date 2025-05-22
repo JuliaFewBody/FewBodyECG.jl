85a034b37194ce30d7bf0e2c6138bb46a8757b7e
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2025-05-22 10:27:44 +0200

    updated docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,8 @@
 using Documenter, FewBodyECG
 
 makedocs(
+    build = "build",
     sitename = "FewBodyECG.jl",
-    format = Documenter.HTML(
-        repolink = "https://github.com/JuliaFewBody/FewBodyECG.jl"
-    ),
-    modules = [FewBodyECG],
     pages = [
         "Home" => "index.md",
         "Theory" => "theory.md",
@@ -13,12 +10,12 @@ makedocs(
         "Resources" => "resources.md",
         "API" => "API.md"
     ],
-    repo = "https://github.com/JuliaFewBody/FewBodyECG.jl",
+    format = Documenter.HTML()
 )
 
 deploydocs(
     repo = "github.com/JuliaFewBody/FewBodyECG.jl",
     target = "build",
     branch = "gh-pages",
-    devbranch = "main"  # or "master" if you're using that
+    devbranch = "main"  
 )
