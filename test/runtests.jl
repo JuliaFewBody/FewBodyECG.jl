3bef56100ff9bdb33ef28c7f7a7cdde2c47078f4
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2026-02-17 15:51:22 +0100

    updated project

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,12 @@ using FewBodyECG
     include("test_coordinates.jl")
     include("test_matrix_elements.jl")
     include("test_hamiltonian.jl")
+<<<<<<< Updated upstream
     include("test_hydrogen.jl")
 
+=======
+    include("test_types.jl")
+    include("test_utils.jl")
+    
+>>>>>>> Stashed changes
 end
